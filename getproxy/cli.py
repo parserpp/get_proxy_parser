5068433c93bf35dff677f885154f8da63f2e9386
sanbo <sanbo.xyz@gmail.com>
2022-01-20 18:45:37 +0800

    update sth. at 2022-01-20 18:45:37

--- getproxy/cli.py
+++ getproxy/cli.py
@@ -12,6 +12,15 @@ def main(in_proxy, out_proxy):
     g = GetProxy(in_proxy, out_proxy)
     g.start()
 
+@click.command()
+@click.option('--in-proxy', help='Input proxy file')
+@click.option('--out-proxy', help='Output proxy file')
+@click.option('--token', help='github token')
+def main(in_proxy, out_proxy, token):
+    if token != "":
+        print("token 有值！！！")
+    g = GetProxy(in_proxy, out_proxy)
+    g.start()
 
 if __name__ == "__main__":
     main()
