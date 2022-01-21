1ed77762796365cf7312e75efbf6b3c93666d50f
sanbo <sanbo.xyz@gmail.com>
2022-01-21 17:36:44 +0800

    update sth. at 2022-01-21 17:36:44

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ import re
 import ast
 from setuptools import setup, find_packages
 
-with open('README.md',  "r", encoding='utf-8') as readme_file:
+with open('README.md', "r", encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-
 with open('__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+    version = str(
+        ast.literal_eval(
+            _version_re.search(f.read().decode('utf-8')).group(1)))
 
 with open('requirements_dev.txt', 'r', encoding='utf-8') as fin:
     requirements = [i.strip() for i in fin.readlines()]
@@ -39,11 +39,7 @@ setup(
     url='https://github.com/parserpp/get_proxy_parser',
     packages=find_packages(),
     package_dir={},
-    entry_points={
-        'console_scripts': [
-            'getproxy=getproxy.cli:main'
-        ]
-    },
+    entry_points={'console_scripts': ['getproxy=getproxy.cli:main']},
     include_package_data=True,
     install_requires=requirements,
     license="BSD license",
