fbf5e37508ebac1e3ced283b67bd9355707f4ac5
sanbo <sanbo.xyz@gmail.com>
2022-01-28 11:46:14 +0800

    update sth. at 2022-01-28 11:46:14

--- getproxy/getproxy.py
+++ getproxy/getproxy.py
@@ -68,22 +68,20 @@ class GetProxy(object):
         request_begin = time.time()
         try:
             usl="%s://httpbin.org/get?show_env=1&cur=%s" % (scheme, request_begin)
-            print(usl)
             response_json = requests.get(
                 usl,
                 proxies=request_proxies,
                 timeout=5
             ).json()
+            if host not in response_json.get('origin', ''):
+                print("[" + host + "]-- _validate_proxy failed" + "----" + usl)
+                return
         except:
             return
 
         request_end = time.time()
 
         if str(request_begin) != response_json.get('args', {}).get('cur', ''):
-            return
-
-        if host not in response_json.get('origin',''):
-            print("[" + host + "]-- _validate_proxy failed")
             return
 
         anonymity = self._check_proxy_anonymity(response_json)
